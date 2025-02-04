e1eef2bf47a2db7f90455807b6e7f1caeebbd946
Navneet Singh <navneet.singh@squareops.com>
2025-02-04 07:57:05 +0000

    code updated

--- versions.tf
+++ versions.tf
@@ -1,16 +1,17 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = ">= 1.3"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.51, < 5.0"
+      version = ">= 4.64, < 7"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 4.51, < 5.0"
+      version = ">= 4.64, < 7"
     }
   }
+
   provider_meta "google" {
-    module_name = "blueprints/terraform/terraform-google-network/v7.0.0"
+    module_name = "blueprints/terraform/terraform-google-network/v10.0.0"
   }
 }
